70ed8b714e104e25f01b024d9ac6587e327b3e24
Gert Cuykens <gert.cuykens@gmail.com>
2019-12-17 01:56:24 +0100

    _

--- doc.sh
+++ doc.sh
@@ -6,3 +6,15 @@
 #   go doc <sym>[.<method>]
 #   go doc [<pkg>].<sym>[.<method>]
 #   go doc <pkg> <sym>[.<method>]
+
+# go get -u github.com/gertcuykens/module/v6
+
+go doc github.com/gertcuykens/module/v6
+go doc github.com/gertcuykens/module/v6 Symbol
+go doc github.com/gertcuykens/module/v6.Symbol
+
+go doc github.com/gertcuykens/module/v6 Symbol.MethodFoo
+# go doc github.com/gertcuykens/module/v6 Symbol MethodFoo
+
+go doc github.com/gertcuykens/module/v6.MethodFoo
+go doc github.com/gertcuykens/module/v6 MethodFoo
